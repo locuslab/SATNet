50897688eae47bf765c1d9ed9a7c6f5419d62a9a
Po-Wei Wang <xflash96@gmail.com>
2022-11-22 01:22:04 -0800

    Updating supported cuda arch to Ampere & docker pytorch version to 1.13

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from torch.utils.cpp_extension import BuildExtension, CppExtension, CUDAExtensio
 from torch.utils.cpp_extension import CUDA_HOME
 
 gencode = [
-        '-gencode=arch=compute_30,code=sm_30',
-        '-gencode=arch=compute_35,code=sm_35',
         '-gencode=arch=compute_50,code=sm_50',
         '-gencode=arch=compute_52,code=sm_52',
         '-gencode=arch=compute_60,code=sm_60',
         '-gencode=arch=compute_61,code=sm_61',
-        '-gencode=arch=compute_61,code=compute_61',
+        '-gencode=arch=compute_75,code=sm_75',
+        '-gencode=arch=compute_80,code=sm_80',
+        '-gencode=arch=compute_86,code=sm_86',
 ]
 
 ext_modules = [
